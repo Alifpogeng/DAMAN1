275557fe12f0b8909c8fc98a837fa2b802afa9b6
Alifpogeng <pogeng45ti@gmail.com>
2021-09-06 16:10:28 +0700

    uye

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/DAMAN/';
+$config['base_url'] = 'https://35bc-180-250-22-66.ngrok.io/DAMAN/';
+// https://35bc-180-250-22-66.ngrok.io/DAMAN
 
 /*
 |--------------------------------------------------------------------------
