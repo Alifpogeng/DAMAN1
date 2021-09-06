cdd080abcffe69fef9ad91364f0d410f6b2d7ef3
Alifpogeng <pogeng45ti@gmail.com>
2021-09-06 13:29:37 +0700

    first commit

---
+++ application/controllers/admin/data_pkl.php
@@ -0,0 +1,16 @@
+<?php
+
+class Data_pkl extends CI_Controller{
+	public function index()
+	{
+		$data['datapkl'] = $this->model_datapkl->tampil_data()->result();
+		// echo print_r($data);
+		// $data['barang'] = $this->model_barang->tampil_data()->result();
+		$this->load->view("templates_admin/header");
+		$this->load->view("templates_admin/sidebar");
+		$this->load->view("admin/data_pkl", $data);
+		$this->load->view("templates_admin/footer");
+
+	}
+} 
+?>
---
+++ application/models/model_datapkl.php
@@ -0,0 +1,11 @@
+<?php 
+ 
+class Model_datapkl extends CI_Model{
+
+	public function tampil_data(){
+		return $this->db->get('tb_data_pkl');
+	}
+	// public function tampil_data(){
+	// 	return $this->db->get('tb_brg');
+	// } 
+}
